1a72962a552361a981a49a670924bd87ecab9f78
Hlib Makarov <makarovhlib@gmail.com>
2021-08-16 17:43:43 +0300

    ToDouble

---
+++ ProgramG.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+
+namespace ProjectG
+{
+    class NewTestProgramm
+    {
+        public static void Main(string[] args)
+        {
+            double a;
+            a = Convert.ToDouble (Console.ReadLine ());
+
+            if (a > 10f) {
+                Console.Write ("A is bigger than 10");
+            } else if (a < 10F){
+                Console.Write ("A  more less than 5");
+            }
+        }
+    }
+}
